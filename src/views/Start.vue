15da4172c7f1f623405a202f2ea9c381bd35b58a
jeklundd <jonas.eklundd@gmail.com>
2021-11-29 14:46:10 +0100

    Min första commit. Ändrat layout på Start samt adderat bilder :)

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,12 +1,28 @@
 <template>
-  <div id="nav">
+
+  <div id="wrapper">
+
+  <header id="header">
+    <h1> Welcome to Pollify</h1>
+    <h3> - Easy to create and participate</h3>
+  </header>
+
+  <div id="langdiv">
+    <img class="langimg" src="img/swe.jpg">
     <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+    <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+    <img class="langimg" src="img/eng.jpg">
+  </div>
+
+  <div id="nav">
     <router-link v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
     <label>
       Write poll id: 
       <input type="text" v-model="id">
     </label>
     <router-link v-bind:to="'/poll/'+id" tag="button">{{uiLabels.participatePoll}}</router-link>
+  </div>
+
   </div>
 </template>
 
@@ -39,3 +55,28 @@ export default {
   }
 }
 </script>
+
+<style>
+
+
+
+.langimg{
+  border-radius: 100%;
+  width: 1.5em;
+  height: 1.5em;
+}
+
+#wrapper {
+  display: grid;
+}
+
+#langdiv{
+  grid-column: 3;
+  grid-row: 1;
+}
+
+#header{
+  grid-column: 2;
+  grid-row: 1;
+}
+</style>
