da998b8d0d57b52338685ff2b150dabb581da1e5
jeklundd <jonas.eklundd@gmail.com>
2021-12-29 20:22:16 +0100

    Colours timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -12,6 +12,16 @@
             cy="50"
             r="46.5"
         />
+        <path
+            :stroke-dasharray="circleDasharray"
+            :class="remainingPathColor"
+            class="path-remaining"
+            d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "></path>
       </g>
     </svg>
     <span class="label">
@@ -28,9 +38,28 @@ export default {
     timeLeft: {
       type: Number,
       required: true
-    }
+    },
+    alertThreshold: {
+      type: Number,
+      default: 5
+    },
+    warningThreshold: {
+      type: Number,
+      default: 10
+    },
   },
   computed: {
+    circleDasharray() {
+      return `${(this.timeFraction * 283).toFixed(0)} 283`;
+    },
+
+    timeFraction() {
+      const rawTimeFraction = this.timeLeft / this.timeLimit
+
+      return rawTimeFraction -
+          (1 / this.timeLimit) * (1 - rawTimeFraction)
+    },
+
     formattedTimeLeft() {
       const timeLeft = this.timeLeft
 
@@ -40,6 +69,33 @@ export default {
         seconds = `0${seconds}`
       }
       return `${minutes}:${seconds}`
+    },
+
+    colorCodes() {
+      return {
+        info: {
+          color: "green"
+        },
+        warning: {
+          color: "orange",
+          threshold: this.warningThreshold
+        },
+        alert: {
+          color: "red",
+          threshold: this.alertThreshold
+        }
+      }
+    },
+
+    remainingPathColor() {
+      const { alert, warning, info } = this.colorCodes;
+      if (this.timeLeft <= alert.threshold) {
+        return alert.color;
+      } else if (this.timeLeft <= warning.threshold) {
+        return warning.color;
+      } else {
+        return info.color;
+      }
     }
   }
 }
@@ -74,6 +130,29 @@ export default {
    font-size: 48px;
 }
 
+.path-remaining {
+   stroke-width: 10px;
+   stroke-linecap: round;
+   transform: rotate(90deg);
+   transform-origin: center;
+   transition: 1s linear all;
+   stroke: currentColor;
+}
 
+.svg {
+transform: scaleX(-1);
+}
+
+.green {
+   color: rgb(65, 184, 131);
+}
+
+.orange {
+   color: orange;
+}
+
+.red {
+   color: red;
+ }
 
 </style>
